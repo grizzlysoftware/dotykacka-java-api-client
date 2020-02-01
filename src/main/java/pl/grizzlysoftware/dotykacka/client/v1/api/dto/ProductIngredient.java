ace162a0cc81d89713ec686f99035e825d9ff7f9
bpawlowski <bpawlowski@grizzlysoftware.pl>
2020-02-01 20:29:09 +0100

    Fixed issue with inproper ProductIngretient mapping

--- src/main/java/pl/grizzlysoftware/dotykacka/client/v1/api/dto/ProductIngredient.java
+++ src/main/java/pl/grizzlysoftware/dotykacka/client/v1/api/dto/ProductIngredient.java
@@ -2,6 +2,7 @@ package pl.grizzlysoftware.dotykacka.client.v1.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * @author Bartosz Pawłowski, bpawlowski@grizzlysoftware.pl
@@ -9,9 +10,18 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProductIngredient {
+    @JsonProperty("ingredienceproductId")
     public Long productId;
+    @JsonProperty("belongstoproductId")
+    public Long parentProductId;
+    @JsonProperty("cloudid")
+    public Long cloudId;
+    @JsonProperty("ingrediencemapid")
     public Long ingredientId;
+    @JsonProperty("deleted")
     public Boolean isDeleted;
+    @JsonProperty("quantity")
     public Integer quantity;
+    @JsonProperty("units")
     public String unit;
 }
