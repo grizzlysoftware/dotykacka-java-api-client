4490e52574b6bf429284c17e306913afd80368cf
bpawlowski <bpawlowski@grizzlysoftware.pl>
2020-02-25 12:46:20 +0100

    Fixed issue with serializer

--- src/main/java/pl/grizzlysoftware/util/NumericBooleanSerializer.java
+++ src/main/java/pl/grizzlysoftware/util/NumericBooleanSerializer.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class NumericBooleanSerializer extends JsonSerializer<Boolean> {
     @Override
     public void serialize(Boolean bool, JsonGenerator generator, SerializerProvider provider) throws IOException, JsonProcessingException {
-        generator.writeString(bool ? "1" : "0");
+        generator.writeNumber(bool ? 1 : 0);
     }   
 }
 
