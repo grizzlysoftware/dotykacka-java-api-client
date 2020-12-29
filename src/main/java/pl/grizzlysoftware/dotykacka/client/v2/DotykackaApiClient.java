94e31a834164bd9b13b2fcc4ab063bd277df08e9
bpawlowski <bpawlowski@grizzlysoftware.pl>
2020-12-29 16:01:07 +0100

    Implemented not fully working client class

--- src/main/java/pl/grizzlysoftware/dotykacka/client/v2/DotykackaApiClient.java
+++ src/main/java/pl/grizzlysoftware/dotykacka/client/v2/DotykackaApiClient.java
@@ -1,7 +1,89 @@
 package pl.grizzlysoftware.dotykacka.client.v2;
 
+import pl.grizzlysoftware.dotykacka.client.v2.api.dto.oauth.OAuthApiToken;
+import pl.grizzlysoftware.dotykacka.client.v2.api.service.*;
+import pl.grizzlysoftware.dotykacka.client.v2.facade.*;
+import pl.grizzlysoftware.dotykacka.model.Configuration;
+import pl.grizzlysoftware.dotykacka.util.AccessTokenProvider;
+import pl.grizzlysoftware.dotykacka.util.ApiTokenProvider;
+import pl.grizzlysoftware.dotykacka.util.DotykackaOAuthAccessTokenExtractor;
+import pl.grizzlysoftware.dotykacka.util.OkHttpAccessTokenAuthenticator;
+import pl.grizzlysoftware.util.OkHttpLoggingInterceptor;
+
+import java.time.Duration;
+
+import static java.util.Objects.requireNonNull;
+import static pl.grizzlysoftware.util.OkHttpClientUtils.builder;
+import static pl.grizzlysoftware.util.RetrofitUtils.service;
+
 /**
  * @author Bartosz Pawłowski, bpawlowski@grizzlysoftware.pl
  */
 public class DotykackaApiClient {
+
+    private final BranchServiceFacade branchService;
+    private final CategoryServiceFacade categoryService;
+    private final CloudServiceFacade cloudService;
+    private final CustomerServiceFacade customerService;
+    private final EmployeeServiceFacade employeeService;
+    private final MoneyLogServiceFacade moneyLogService;
+    private final OAuthServiceFacade oauthService;
+    private final OrderItemServiceFacade orderItemService;
+    private final OrderServiceFacade orderService;
+    private final ProductCustomizationServiceFacade productCustomizationService;
+    private final ProductIngredientServiceFacade productIngredientService;
+    private final ProductServiceFacade productService;
+    private final ReservationServiceFacade reservationService;
+    private final SupplierServiceFacade supplierService;
+    private final TableServiceFacade tableService;
+    private final TagServiceFacade tagService;
+    private final WarehouseBranchServiceFacade warehouseBranchService;
+    private final WarehouseServiceFacade warehouseService;
+
+    protected Configuration configuration;
+    protected AccessTokenProvider accessTokenProvider;
+    protected ApiTokenProvider apiTokenProvider;
+
+    public DotykackaApiClient(Configuration configuration) {
+        this.configuration = requireNonNull(configuration).clone();
+        var oAuthHttpClient = builder()
+                .callTimeout(configuration.requestTimeout != null ? configuration.requestTimeout : Duration.ofSeconds(60))
+                .retryOnConnectionFailure(true)
+                .addInterceptor(new OkHttpLoggingInterceptor())
+                .build();
+//        oauthService = new OAuthServiceFacade(service(oAuthHttpClient, configuration.url, OAuthService.class));
+        oauthService = new OAuthServiceFacade();
+
+//        if (configuration.apiTokenAuthCredentials == null) {
+//            apiTokenProvider = new ApiTokenProvider(oauthService, new OAuthApiToken(configuration.apiToken));
+//        } else {
+//            apiTokenProvider = new ApiTokenProvider(oauthService, configuration.apiTokenAuthCredentials);
+//        }
+//        accessTokenProvider = new AccessTokenProvider(oauthService, apiTokenProvider, configuration.accessTokenAuthCredentials, configuration.apiToken, configuration.oAuthTokenValidationTolerance);
+
+        var authenticator = new OkHttpAccessTokenAuthenticator(accessTokenProvider, new DotykackaOAuthAccessTokenExtractor());
+        var secureServiceHttpClient = builder()
+                .addInterceptor(authenticator)
+                .authenticator(authenticator)
+                .addInterceptor(new OkHttpLoggingInterceptor())
+                .build();
+
+        branchService = new BranchServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, BranchService.class));
+        cloudService = new CloudServiceFacade(service(secureServiceHttpClient, configuration.url, CloudService.class));
+        categoryService = new CategoryServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, CategoryService.class));
+        customerService = new CustomerServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, CustomerService.class));
+        employeeService = new EmployeeServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, EmployeeService.class));
+        orderItemService = new OrderItemServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, OrderItemService.class));
+        orderService = new OrderServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, OrderService.class));
+        moneyLogService = new MoneyLogServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, MoneyLogService.class));
+        productService = new ProductServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, ProductService.class));
+        productCustomizationService = new ProductCustomizationServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, ProductCustomizationService.class));
+        productIngredientService = new ProductIngredientServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, ProductIngredientService.class));
+        reservationService = new ReservationServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, ReservationService.class));
+        supplierService = new SupplierServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, SupplierService.class));
+        tableService = new TableServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, TableService.class));
+        tagService = new TagServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, TagService.class));
+        warehouseService = new WarehouseServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, WarehouseService.class));
+        warehouseBranchService = new WarehouseBranchServiceFacade(configuration.cloudId, service(secureServiceHttpClient, configuration.url, WarehouseBranchService.class));
+    }
 }
---
+++ src/main/java/pl/grizzlysoftware/dotykacka/client/v2/facade/OAuthServiceFacade.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 Grizzly Software, https://grizzlysoftware.pl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package pl.grizzlysoftware.dotykacka.client.v2.facade;
+
+import pl.grizzlysoftware.dotykacka.client.v1.api.dto.oauth.OAuthAccessToken;
+import pl.grizzlysoftware.dotykacka.client.v1.api.dto.oauth.OAuthApiToken;
+import pl.grizzlysoftware.dotykacka.client.v1.api.service.OAuthService;
+import pl.grizzlysoftware.util.RetrofitApiServiceFacade;
+
+import static java.lang.System.currentTimeMillis;
+import static java.util.Base64.getEncoder;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * @author Bartosz Pawłowski, bpawlowski@grizzlysoftware.pl
+ */
+public class OAuthServiceFacade extends RetrofitApiServiceFacade {
+    private OAuthService service;
+
+    public OAuthServiceFacade(OAuthService service) {
+        this.service = requireNonNull(service);
+    }
+
+    public OAuthServiceFacade() {
+        //TODO to be removed when actual implementation of OAuthService is there
+    }
+
+    public OAuthAccessToken accessToken(String username, String password, String apiToken) {
+        var token = execute(service.accessToken(apiToken, "", "password", basicEncoded(username, password)));
+        token.receivedAt = currentTimeMillis();
+        return token;
+    }
+
+    public OAuthApiToken apiToken(String username, String password) {
+        return execute(service.apiToken(username, password, "touchpo"));
+    }
+
+    private String basicEncoded(String username, String password) {
+        return "Basic "+ new String(getEncoder().encode(String.format("%s:%s", username, password).getBytes()));
+    }
+}
